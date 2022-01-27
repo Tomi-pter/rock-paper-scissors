b0d10659ac535f8de7dd826dbf7ea6546211f41e
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-01-27 15:48:51 +0100

    rule component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Score from "./components/Score";
 import Game from "./components/Game";
+import Rules from "./components/Rules";
 
 function App() {
   return (
     <>
       <Score />
       <Game />
+      <Rules />
     </>
   );
 }
---
+++ src/components/Rules.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import rules from "../images/image-rules.svg";
+import closeBtn from "../images/icon-close.svg";
+import { useState } from "react";
+
+const RulesBtn = styled.button`
+  background-color: rgb(255, 255, 255, 0);
+  padding: 0.5rem 2rem;
+  margin: 1rem 0 1rem 90%;
+  color: white;
+  border: 1px solid white;
+  border-radius: 0.35rem;
+  font-family: "BSB";
+`;
+
+const RulesImg = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  height: auto;
+  background-color: white;
+  margin: 0 auto;
+  top: 25vh;
+  transform: translateY(-175%);
+  padding: 0 2rem 1rem;
+  border-radius: 0.5rem;
+  z-index: 20;
+  position: relative;
+
+  h1 {
+    font-size: 1.4rem;
+  }
+  .headClose {
+    display: flex;
+    justify-content: space-between;
+    font-family: "BB";
+    align-items: center;
+  }
+  button {
+    padding: 0;
+    height: 1.5rem;
+    border: none;
+    background-color: transparent;
+  }
+  img {
+    max-width: 15rem;
+    margin: auto;
+  }
+`;
+
+function Rules() {
+  const [ruleDisplay, setRuleDisplay] = useState(false);
+
+  const displayRule = () => {
+    setRuleDisplay(true);
+  };
+
+  const removeRule = () => {
+    setRuleDisplay(false);
+  };
+
+  return (
+    <>
+      <RulesBtn onClick={displayRule}>RULES</RulesBtn>
+      {ruleDisplay && (
+        <>
+          <RulesImg>
+            <div className="headClose">
+              <h1>RULES</h1>
+              <button onClick={removeRule}>
+                <img src={closeBtn} alt="close" />
+              </button>
+            </div>
+            <img src={rules} alt="" />
+          </RulesImg>
+        </>
+      )}
+    </>
+  );
+}
+
+export default Rules;
