d556da9e4d5909a3d364aabf6553739c45f24ac0
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-01-31 10:57:25 +0100

    fixed score bug

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -183,14 +183,15 @@ function Game({ score, setScore }) {
 
   if (scoreRef.current === "Stalemate") {
     setScore(+dispNum);
-    totalRef.current = score;
   } else if (scoreRef.current === "You win") {
     setScore(+dispNum + 1);
-    totalRef.current = score;
   } else if (scoreRef.current === "You lose") {
     setScore(+dispNum - 1);
-    totalRef.current = score;
   }
+  useEffect(() => {
+    totalRef.current = score;
+  }, [score]);
+
   const updateScore = () => {
     setDispNum(totalRef.current);
     scoreRef.current = "";
